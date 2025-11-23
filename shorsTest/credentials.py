8866788f7f94fefda19d8b3aae3f0593a2d0f0ac
Life220 <fberrino.pro@gmail.com>
2025-11-23 20:33:03 +0200

    Credential file fix

--- shorsTest/credentials.py
+++ shorsTest/credentials.py
@@ -1,13 +1,7 @@
 from qiskit_ibm_runtime import QiskitRuntimeService
 
-# Create a my_credentials.py as a duplicate of this file
 QiskitRuntimeService.save_account(
-token="<your-api-key>", # Use the 44-character API_KEY you created and saved from the IBM Quantum Platform Home dashboard
-instance="<CRN>", # Optional
+    channel="ibm_quantum_platform",
+    token="<token>",
+    overwrite=True
 )
-
-# To use in a .py
-from qiskit_ibm_runtime import QiskitRuntimeService
-
-# Run every time you need the service
-service = QiskitRuntimeService()
